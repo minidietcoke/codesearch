ccdd817de05d4cd5e5a7e456ec4270d8120214ef
Mary Lin <mary@phntms.com>
2017-02-18 19:18:50 +0000

    Check user inputted file path is valid
    - Move some parts into functions

--- search.py
+++ search.py
@@ -1,38 +1,58 @@
 import os
+import sys
 from os.path import splitext
+
 
 def main():
     print 'What is the path of the codebase folder:'
     codebase = raw_input()
+    try:
+	os.stat(codebase)
+    except:
+	print 'Invalid path, please start again'
+	sys.exit()
     print 'What is the search term:'
-    search_term = raw_input()
+    word = raw_input()
     print 'What file type you like to search (.py, .js etc or leave empty):'
-    file_ext = raw_input()
+    ext = raw_input()
 
-    search_word(codebase, search_term, file_ext)
+    results = find_word(codebase, word, ext)
+    print_results(results)
 
-
-def search_word(codebase, search_term, file_ext):
-    if file_ext:
-	if file_ext[0] != '.':
-	    file_ext = '.{}'.format(file_ext)
+def find_word(codebase, word, ext=''):
+    if ext and ext[0] != '.':
+	ext = '.{}'.format(ext)
 
     results = []
 
-    for subdir, dirs, files in os.walk(codebase):
-	for file in files:
-	    if splitext(file)[1] == file_ext or file_ext == '':
-		file_path = os.path.join(subdir, file)
-		f = open(file_path)
-		file_result = []
-	    	for i, line in enumerate(f):
-		    if search_term in line:
-		    	file_result.append('\t{line_num}: {line}'.format(
-			    line_num=i, line=line.strip()))
-		if len(file_result) > 0:
-		    file_result.insert(0, file_path)
-		    results.append(file_result)
+    try:
+	file = open(codebase)
+	results.extend(find_word_in_file(word, file))
+    except IOError:
+	for subdir, dirs, files in os.walk(codebase):
+	    for file in files:
+		if is_matching_file_ext(file, ext):
+		    file_path = os.path.join(subdir, file)
+		    f = open(file_path)
+		    file_result = find_word_in_file(word, f)
+		    if len(file_result) > 0:
+			file_result.insert(0, file_path)
+			results.append(file_result)
 
+    return results
+
+def find_word_in_file(word, file):
+    file_result = []
+    for i, line in enumerate(file):
+	if word in line:
+	    file_result.append(
+		'\t{line_num}: {line}'.format(line_num=i, line=line.strip()))
+    return file_result
+
+def is_matching_file_ext(file, file_ext):
+    return splitext(file)[1] == file_ext or file_ext == ''
+
+def print_results(results):
     for result in results:
 	print '"""'
 	print '\n'.join(result)
